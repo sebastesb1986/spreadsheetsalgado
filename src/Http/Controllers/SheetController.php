47c5db05a1cd011c766b2388653b619701b94ae9
unknown <sebastesb1986@gmail.com>
2020-03-10 19:13:03 -0500

    custom spreadsheet excel

--- src/Http/Controllers/SheetController.php
+++ src/Http/Controllers/SheetController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Devsheet\Spreadsheet\imports\SpreadSheet;
+use App\imports\SpreadSheet;
 use App\Sheet;
 use App\SheetHeader;
 use Maatwebsite\Excel\Facades\Excel;
--- src/models/SheetHeader.php
+++ src/models/SheetHeader.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Devsheet\Spreadsheet\models;
+namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
